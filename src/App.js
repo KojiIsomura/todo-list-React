f257373b349d1085defd7655808fb0a80b8ac44a
KojiIsomura <70617695+KojiIsomura@users.noreply.github.com>
2020-12-21 10:07:00 +0900

    20201221-2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => {
-    alert();
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowflag] = useState(true);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowflag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowflag(true);
+    } else {
+      faceShowFlag && setFaceShowflag(false);
+    }
+  }, [num]);
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">aaaaa</ColorfulMessage>
-      <button onClick={onClickButton}>button</button>
+      <button onClick={onClickCountUp}>count up</button>
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>(´・ω・｀)</p>}
     </>
   );
 };
